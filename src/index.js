0cc71bd8c82f389e794f1d61a0742aa256ff7bac
sega11456 <sega114567@gmail.com>
2021-04-10 16:05:58 +0700

    login page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const myfirstelement = <h1>Hello BorntoDev :)</h1>
-
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+import logo from './logokmutt.png';
+import './index.css';
+import {Media} from 'react-bootstrap';
+import { render } from '@testing-library/react';
+import { useMediaQuery } from 'react-responsive'
+ 
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
+}
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
+  return isTablet ? children : null
+}
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
+const Default = ({ children }) => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 })
+  return isNotMobile ? children : null
+}
+ 
+const Example = () => (
+  <div>
+    <Desktop>Desktop or laptop</Desktop>
+    <Tablet>Tablet</Tablet>
+    <Mobile>Mobile</Mobile>
+    <Default>Not mobile (desktop or laptop or tablet)</Default>
+  </div>
+)
+ 
+export default Example
+  
